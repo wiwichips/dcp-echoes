ed03babfc71d76b9c24041ab55c619ef2ad5490c
Will Pringle <will@distributive.network>
2024-02-18 09:41:57 -0500

    feat(dcp-echoes): initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * @file    index.js
+ *          Entrypoint for the dcp-echoes package. Returns the result
+ *          of `require('dcp-client').initSync();` even if it has
+ *          already been invoked.
+ *
+ *          NOTE: has the side effect of invoking dcp-client init if it
+ *          has not been invoked yet!!! You have been warned!!!
+ *
+ * @author  Will Pringle <willkantorpringle@gmail.com>
+ * @date    February 2024
+ */
+
+var dcp = null;
+
+if ('dcp/build' in require.cache)
+{
+  // these modules are not part of the object initSync returns
+  const denyList = [
+    'dcp/bank-util',
+    'dcp/bootstrap-build',
+    'dcp/client',
+    'dcp/client-bundle',
+    'dcp/env-native',
+    'dcp/internal/dcp-default-config',
+    'dcp/internal/debugging',
+    'dcp/internal/engine-constructors',
+    'dcp/internal/job-values'
+  ];
+
+  // decorate with identical properties to initSync
+  dcp = {};
+  for (const name in require.cache)
+  {
+    if (!name.startsWith('dcp/') || denyList.includes(name))
+      continue;
+
+    const modName = name.slice(4);
+
+    dcp[modName] = require.cache[name]['exports'];
+  }
+}
+else
+  dcp = require('dcp-client').initSync();
+
+module.exports = dcp;
+
